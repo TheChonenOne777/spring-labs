072f4dbdfc459af67a15a0d75aea389a015b0a81
VladimirChertilov <vladimir.chertilov@epam.com>
2017-10-18 23:45:05 +0300

    lab6, lab7 added lombok annotations

--- lab-7-orm/src/main/java/lab/model/Country.java
+++ lab-7-orm/src/main/java/lab/model/Country.java
@@ -1,7 +1,16 @@
 package lab.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Country implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -11,66 +20,4 @@ public class Country implements Serializable {
 	private String name;
 
 	private String codeName;
-
-	public Country() {
-	}
-
-	public Country(int id, String name, String codeName) {
-		this.id = id;
-		this.name = name;
-		this.codeName = codeName;
-	}
-
-	public Country(String name, String codeName) {
-		this.name = name;
-		this.codeName = codeName;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getCodeName() {
-		return codeName;
-	}
-
-	public void setCodeName(String codeName) {
-		this.codeName = codeName;
-	}
-
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
-
-		Country country = (Country) o;
-
-		return (codeName != null ? codeName.equals(country.codeName) : country.codeName == null) && (name != null ? name.equals(country.name) : country.name == null);
-	}
-
-	public int hashCode() {
-		int result = 0;
-		result = 31 * result + (name != null ? name.hashCode() : 0);
-		result = 31 * result + (codeName != null ? codeName.hashCode() : 0);
-		return result;
-	}
-
-	@Override
-	public String toString() {
-		return "Country [id=" + id + ", name=" + name + ", codeName="
-				+ codeName + "]";
-	}
 }
